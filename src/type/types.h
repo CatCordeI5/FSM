c2485168bdc9412d09849e1d8a1063047ffe5622
CatCordeI5 <jokopurwantonew2@gmail.com>
2026-08-26 20:43:24 +0700

    Create types.h

---
+++ src/type/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+typedef unsigned long long uint64_t;
+
+#define NULL ((void*)0)
+#define TRUE 1
+#define FALSE 0
+
+#endif
