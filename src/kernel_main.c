d672e76cf4a0784b9eef20d717a987bc16dce93f
CatCordeI5 <jokopurwantonew2@gmail.com>
2026-08-26 21:37:13 +0700

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,5 +1,33 @@
 #include <stdint.h>
 
+extern void kprint(const char* str);
+extern void put_char(char c);
+extern char get_key_raw();
+extern void shifthat_init();
+extern void shifthat_scan();
+extern adl_binary_t loom_compile(char* source_code);
+extern void handle_spawn_command(char* cmd_str);
+extern char process_alien_key(char mod, char target);
+
+void kprint(const char* str) {
+    volatile uint16_t* vga = (uint16_t*)0xB8000;
+    int i = 0;
+    while (str[i] != '\0') {
+        vga[i] = (0x0F << 8) | str[i];
+        i++;
+    }
+}
+
+void put_char(char c) {
+    volatile uint16_t* vga = (uint16_t*)0xB8000;
+    static int cursor = 0;
+    vga[cursor++] = (0x0F << 8) | c;
+}
+
+char get_key_raw() {
+    return 0; 
+}
+
 void _start() {
     kprint("FSM: FUCK SYSTEM-LIKE INITIALIZED\n");
     shifthat_init();
@@ -9,14 +37,7 @@ void _start() {
         
         char mod = get_key_raw();
         if(mod == '~') {
-            char cmd_buffer[100];
-            read_command(cmd_buffer);
-            
-            if(strstr(cmd_buffer, "set spawn point")) {
-                handle_spawn_command(cmd_buffer);
-            } else if (strstr(cmd_buffer, "loom")) {
-                loom_compile(load_file_from_disk(cmd_buffer));
-            }
+            kprint("\n[COMMAND MODE]\n");
         } else {
             char target = get_key_raw();
             char result = process_alien_key(mod, target);
